46046993dfffff97387cd6b5f0b6576fbefa4abd
Henrique Krever <65780366+Henrique-Liesenfeld-Krever@users.noreply.github.com>
2021-07-22 20:36:57 -0300

    Update outroRecursão.c

--- outroRecursão.c
+++ outroRecursão.c
@@ -7,10 +7,9 @@ void recursiva(int num)
         return;
     }
     
-    recursiva(num-1);
-    
+    recursiva(num-1);               // aqui o print vai manda 1 2 3 4 5
     printf("%d ",num);
-
+    //recursiva(num-1);             // se a chamada da função recursiva estiver aqui o printf manda 5 4 3 2 1
 }
 
 int main()
